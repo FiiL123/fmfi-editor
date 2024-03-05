afec709202a716f69bec836dfcd4b08533b546a6
Filipko^^ <fiilgameslp@gmail.com>
2024-03-05 09:42:58 +0100

    make canvas moovable and zoomable

--- editor/main/urls.py
+++ editor/main/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -6,3 +8,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("home", views.home, name="home"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
