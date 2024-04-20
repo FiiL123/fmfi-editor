beee3e8c8f4b5020b5c4f44fe407a378a44bd29b
Filipko^^ <fiilgameslp@gmail.com>
2024-04-20 15:24:58 +0200

    add dept, item and purpose models

--- editor/main/models.py
+++ editor/main/models.py
@@ -16,4 +16,51 @@ class PartAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Part._meta.fields if field.name != "id"]
 
 
+class Department(models.Model):
+    MATHEMATICAL = "mathematical"
+    PHYSICAL = "physical"
+    INFORMATICAL = "informatical"
+    DIDACTIC = "didactic"
+    SUPPORT = "support"
+
+    DEPARTMENT_TYPES = [
+        (MATHEMATICAL, "Mathematical"),
+        (PHYSICAL, "Physical"),
+        (INFORMATICAL, "Informatical"),
+        (DIDACTIC, "Didactic"),
+        (SUPPORT, "Support"),
+    ]
+
+    id = models.CharField(max_length=200, primary_key=True)  # Assuming the id is unique
+    type = models.CharField(max_length=20, choices=DEPARTMENT_TYPES)
+
+    def __str__(self):
+        return self.id
+
+
+class Item(models.Model):
+    id = models.CharField(max_length=100, primary_key=True)
+    text = models.CharField(max_length=100)
+    icon = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.id
+
+
+class Purpose(models.Model):
+    id = models.CharField(max_length=100, primary_key=True)
+    text = models.CharField(max_length=100)
+    icon = models.CharField(max_length=100, blank=True, null=True)
+    colour = models.CharField(max_length=20, blank=True, null=True)
+    interesting = models.BooleanField(default=False)
+    free_room_candidate = models.BooleanField(default=False)
+    default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+
+
 admin.site.register(Part)
+admin.site.register(Department)
+admin.site.register(Item)
+admin.site.register(Purpose)
